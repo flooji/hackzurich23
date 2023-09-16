d8927f8f4bdca34784da409653de19c3bb9b28b1
Matan <a@a>
2023-09-16 15:22:27 +0300

    Migros products

--- ChatGPT.py
+++ ChatGPT.py
@@ -9,6 +9,16 @@ def prompt_chatgpt(prompt, history=[]):
                                                          )
     return {"role": completion['choices'][0]['message']['role'], "content": completion['choices'][0]['message']['content']}
 
+
+def get_suggestions(ingredient, product_list):
+    suggestions = []
+    for pro in product_list:
+        if " " + ingredient.lower() + " " in pro['name'].lower():
+            suggestions.append(pro)
+    return suggestions if len(suggestions) > 0 else ingredient
+
+
+
 if __name__ == "__main__":
     # shakshuka = prompt_chatgpt("Give me a Shaksuhka recipe.")
     # print(shakshuka['content'])
@@ -19,6 +29,9 @@ if __name__ == "__main__":
     # second_step = prompt_chatgpt(history=[shakshuka, first_step], prompt="Next step.")
     # print(second_step['content'])
 
-    products = glob.glob("/Users/matan/Documents/HackZurich23/Migros_case/products/en/*")
-    print(" ")
+    with open("/Users/matan/Documents/HackZurich23/Migros_case/products/all_products.json") as f:
+        all_products = json.load(f)
+
+    suggestions = get_suggestions("rice", all_products)
+    print(suggestions)
 
