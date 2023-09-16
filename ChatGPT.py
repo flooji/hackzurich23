542fcbc1a8ea2a1f7802550c1a86cb044739a873
Matan <a@a>
2023-09-16 15:14:17 +0300

    Migros products

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,4 +1,6 @@
 import openai
+import glob
+import json
 openai.api_key = "KEY"
 MODEL = "gpt-3.5-turbo"
 
@@ -8,11 +10,15 @@ def prompt_chatgpt(prompt, history=[]):
     return {"role": completion['choices'][0]['message']['role'], "content": completion['choices'][0]['message']['content']}
 
 if __name__ == "__main__":
-    shakshuka = prompt_chatgpt("Give me a Shaksuhka recipe.")
-    print(shakshuka['content'])
-    #
-    first_step = prompt_chatgpt(history=[shakshuka], prompt="Give me the first step instruction")
-    print(first_step['content'])
-    #
-    second_step = prompt_chatgpt(history=[shakshuka, first_step], prompt="Next step.")
-    print(second_step['content'])
+    # shakshuka = prompt_chatgpt("Give me a Shaksuhka recipe.")
+    # print(shakshuka['content'])
+    # #
+    # first_step = prompt_chatgpt(history=[shakshuka], prompt="Give me the first step instruction")
+    # print(first_step['content'])
+    # #
+    # second_step = prompt_chatgpt(history=[shakshuka, first_step], prompt="Next step.")
+    # print(second_step['content'])
+
+    products = glob.glob("/Users/matan/Documents/HackZurich23/Migros_case/products/en/*")
+    print(" ")
+
